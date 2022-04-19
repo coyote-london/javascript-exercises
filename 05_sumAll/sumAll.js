69016907cfeb4397c455149be1005426111c5e1f
Reid Jernigan <reidlondonjernigan@gmail.com>
2022-04-19 13:36:23 -0500

    Finish exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
-
+const sumAll = function(numX, numY) {
+    let sum = 0; 
+    if (numX < 0 || numY < 0) {
+        sum = 'ERROR';
+    }
+    else if (typeof numX != 'number' || typeof numY != 'number') {
+        sum = 'ERROR';
+    }
+    else if (numX < numY) {
+        for (i = numX; i<=numY; i++) {
+            sum = sum + i;
+        }
+    }
+    else if (numY < numX) {
+        for (i= numY; i<=numX; i++) {
+            sum = sum + i;
+        }
+    }
+    return sum;
 };
 
 // Do not edit below this line
