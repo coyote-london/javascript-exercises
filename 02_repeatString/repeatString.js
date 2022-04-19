e36493db7ea1822d069cf56c0f982fb6883db2c5
Reid Jernigan <reidlondonjernigan@gmail.com>
2022-04-18 21:29:12 -0500

    complete exercises 01 and 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let stringRepeated = ''
+    for (i=0; i < num; i++) {
+        stringRepeated = stringRepeated + string;
+    }
+    return stringRepeated;
 };
 
 // Do not edit below this line
