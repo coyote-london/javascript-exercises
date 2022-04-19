d1d999de29c388c81bc748a6e056d855362fec58
Reid Jernigan <reidlondonjernigan@gmail.com>
2022-04-19 12:46:42 -0500

    Finish exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, a, b = 0, c =0, d = 0) {
+    let removeTheseItems = [a, b, c, d];
+    for (i=0; i<removeTheseItems.length; i++) {
+        if (array.includes(removeTheseItems[i])) {
+            array.splice(array.indexOf(removeTheseItems[i]), 1);
+        }
+        else {
+            continue;
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
