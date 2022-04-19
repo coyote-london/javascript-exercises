5d1cfad717e64dcfd9e5bf5b8e36854e38b24f77
Reid Jernigan <reidlondonjernigan@gmail.com>
2022-04-19 15:14:01 -0500

    Finish exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(num) {
+  num = (num-32)/1.8*10;
+  num = Math.round(num);
+  return num/10;
 };
 
-const ctof = function() {
-
+const ctof = function(num) {
+  num = (num*1.8+32)*10;
+  num = Math.round(num);
+  return num/10;
 };
 
 // Do not edit below this line
