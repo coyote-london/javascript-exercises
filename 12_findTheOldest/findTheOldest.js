79071972d5b2cc7c8479ccea3ff9870b0057c3d2
Reid Jernigan <reidlondonjernigan@gmail.com>
2022-05-11 13:58:22 -0500

    Finish exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+  return people.reduce((output, currentPerson) => {
+    const oldestAge = getAge(output.yearOfBirth, output.yearOfDeath);
+    const currentAge = getAge(currentPerson.yearOfBirth, currentPerson.yearOfDeath);
+    return oldestAge < currentAge ? currentPerson : output;
+  })
+};
 
+const getAge = function(birth, death) {
+  if (!death) {
+    death = new Date().getFullYear();
+  }
+  return death - birth;
 };
 
 // Do not edit below this line
