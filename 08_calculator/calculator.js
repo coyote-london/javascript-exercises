0685581c7cb3b28d7cfc084fc61e1c36f3e92868
Reid Jernigan <reidlondonjernigan@gmail.com>
2022-05-10 21:29:40 -0500

    Finish exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
+const add = function(num1, num2) {
+  return (num1 + num2);
+};
+
+const subtract = function(num1, num2) {
+  return (num1 - num2);
 	
 };
 
-const subtract = function() {
-	
-};
-
-const sum = function() {
-	
-};
-
-const multiply = function() {
+const sum = function(array) {
+  if (array.length < 1){
+    return 0;
+  }
+  else {
+    return array.reduce((a, b) => a + b);
+  }
 
 };
 
-const power = function() {
+const multiply = function(array) {
+  if (array.length < 3) return array[0] * array[1];
+  else return array.reduce((a,b)=> a * b);
+
+};
+
+const power = function(base, exponate) {
+  return Math.pow(base, exponate);
 	
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let output = num;
+  if (num == 1 || num == 0) return 1;
+  else 
+    for (i = num - 1; i > 1; i--) {
+      output = output * i;
+    }
+  return output;
 };
 
 // Do not edit below this line
