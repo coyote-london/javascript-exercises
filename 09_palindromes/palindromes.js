e8c158d9eb10ed9a120d3c38df9dfefa57086975
Reid Jernigan <reidlondonjernigan@gmail.com>
2022-05-10 23:15:47 -0500

    Finish exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
-
+const palindromes = function(string) {
+    let letters = 'abcdefghijklmnopqrstuvwxyz'
+    letters = letters.split('');
+    let output = [];
+    string = string.toLowerCase().split('');
+    let answer = true;
+    for (i=0; i < string.length; i++) {
+        if (letters.includes(string[i])) {
+            output.push(string[i]);
+        }
+    }
+    return output.join('') === output.reverse().join('');
 };
 
 // Do not edit below this line
