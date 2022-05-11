f7a084f8570e4f3a90e7baf7c264494106db32ee
Reid Jernigan <reidlondonjernigan@gmail.com>
2022-05-11 11:31:37 -0500

    Finish exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    n = parseInt(n);
+    if (n < 0) return 'OOPS';
+    else if (n == 1) return 1;
+    let firstNum = 0;
+    let secondNum = 1;
+    let output;
+    for (i=0; i < n - 1; i++) {
+        i % 2 == 0 ? firstNum = firstNum + secondNum : secondNum = secondNum + firstNum;
+        i % 2 == 0 ? output = firstNum : output = secondNum;
+    }
+    return output;
 };
 
 // Do not edit below this line
